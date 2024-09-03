a95281a08a3d3901039f35d188f53b0316f8f942
Mihir Makwana <mihirpmakwana786@gmail.com>
2024-09-03 15:30:14 +0530

    Solutions of L02/1-2-3

---
+++ solutions/02-first-java/L02_two.java
@@ -0,0 +1,13 @@
+//2. Take name as input and print a greeting message for that particular name.
+
+import java.util.*;
+
+public class L02_two {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter your name: ");
+		String name = sc.next();
+		System.out.println("Welcome, " + name);
+	}
+}
